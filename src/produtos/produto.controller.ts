02493b5eea0706fdb43d41455427d08fdbb80bfe
joaoreider <joaopauloj405@gmail.com>
2023-03-02 17:44:12 -0300

    Finalizando modulo produtos

--- src/produtos/produto.controller.ts
+++ src/produtos/produto.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { randomUUID } from "crypto";
+import { AtualizaProdutoDTO } from "./dto/AtualizaProduto.dto";
+import { CriaProdutoDTO } from "./dto/CriaProduto.dto";
+import { ListaProdutoDTO } from "./dto/listaProduto.dto";
+import { ProdutoEntity } from "./produto.entity";
+
 import { ProdutoRepository } from "./produto.repository";
 
 @Controller('/produtos')
@@ -7,16 +13,53 @@ export class ProdutoController {
     constructor(private readonly produtoRepository: ProdutoRepository){}
 
     @Post()
-    async criaProduto(@Body() dadosdoProduto){
+    async criaProduto(@Body() dadosProduto: CriaProdutoDTO){
 
-       const produtoCadastrado =  this.produtoRepository.salvar(dadosdoProduto)
-       return produtoCadastrado;
+      const produto = new ProdutoEntity();
 
+      produto.id = randomUUID();
+      produto.nome = dadosProduto.nome;
+      produto.usuarioId = dadosProduto.usuarioId;
+      produto.valor = dadosProduto.valor;
+      produto.quantidade = dadosProduto.quantidade;
+      produto.descricao = dadosProduto.descricao;
+      produto.categoria = dadosProduto.categoria;
+      produto.caracteristicas = dadosProduto.caracteristicas;
+      produto.imagens = dadosProduto.imagens;
+      
+
+      const produtoCadastrado =  this.produtoRepository.salvar(produto)
+      return produtoCadastrado
     }
 
     @Get()
     listaProdutos() {
       return this.produtoRepository.listar();
     }
+
+    @Put('/:id')
+    async atualiza(@Param('id') id: string, @Body() dadosProduto: AtualizaProdutoDTO){
+
+      const produtoAlterado = await this.produtoRepository.atualiza(
+        id,
+        dadosProduto,
+      );
+
+      return {
+        mensagem: 'produto atualizado com sucesso',
+        produto: produtoAlterado,
+      };
+
+    }
+
+  @Delete('/:id')
+  async remove(@Param('id') id: string) {
+    const produtoRemovido = await this.produtoRepository.remove(id);
+
+    return {
+      mensagem: 'produto removido com sucesso',
+      produto: produtoRemovido,
+    };
+  }
     
 }
--- src/produtos/produto.repository.ts
+++ src/produtos/produto.repository.ts
@@ -1,12 +1,52 @@
+import { Injectable } from "@nestjs/common";
+import { ProdutoEntity } from "./produto.entity";
 
+@Injectable()
 export class ProdutoRepository{
-    private produtos = [];
+    private produtos: ProdutoEntity[] = [];
 
-    async salvar(produto){
-       this.produtos.push(produto)
+    private buscaPorId(id: string){
+        const possivelProduto = this.produtos.find(
+            produto => produto.id === id
+        )
+        if(!possivelProduto){
+            throw new Error('Produto não encontrado')
+        }
+
+        return possivelProduto
+    }
+
+    async salvar(DadosProduto: ProdutoEntity){
+       this.produtos.push(DadosProduto)
+       return DadosProduto
     }
 
     async listar(){
         return this.produtos
     }
+
+    async atualiza(id: string, dadosProduto: Partial<ProdutoEntity>){
+        const dadosNaoAtualizaveis = ['id', 'usuarioId'];
+        const produto = this.buscaPorId(id);
+
+        Object.entries(dadosProduto).forEach(([chave, valor]) => {
+            if (dadosNaoAtualizaveis.includes(chave)) {
+              return;
+            }
+            produto[chave] = valor;
+          });
+
+          return produto
+
+    }
+
+    async remove(id: string) {
+        const produtoRemovido = this.buscaPorId(id);
+        this.produtos = this.produtos.filter((produto) => produto.id !== id);
+        return produtoRemovido;
+      }
+
+
+
+
 }
